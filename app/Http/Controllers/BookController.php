f21b49d5bcbbe08f30a0ee5a13b302ec0506db56
sak <samudra_ajri@live.com>
2021-03-09 23:54:16 +0700

    add book model, controller, apiResources

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -2,62 +2,44 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Book;
 use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class BookController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
-        //
+        return Book::all();
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
-        //
+        $book = Book::create(
+            $request->only('title', 'release_year', 'description')
+        );
+
+        return response($book, Response::HTTP_CREATED);
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function show($id)
     {
-        //
+        return Book::find($id);
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function update(Request $request, $id)
     {
-        //
+        $book = Book::find($id);
+
+        $book->update($request->only('title', 'release_year', 'description'));
+
+        return response($book, Response::HTTP_ACCEPTED);
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function destroy($id)
     {
-        //
+        Book::destroy($id);
+
+        return response(null, Response::HTTP_NO_CONTENT);
     }
 }
