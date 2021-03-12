ecd661510bf3eda75040319d7e071d73c57dec73
sak <samudra_ajri@live.com>
2021-03-12 14:03:05 +0700

    add author seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,6 +15,8 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             BookSeeder::class,
+            CategorySeeder::class,
+            AuthorSeeder::class,
         ]);
     }
 }
